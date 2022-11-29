d38b8c465d5eef4aef2481efcbaa18209cf70a32
Emma Gronstad <gronstad@usc.edu>
2022-11-29 07:43:14 +0000

    Add files via upload

--- ee450_gronstad_emma_gronstad/client.cpp
+++ ee450_gronstad_emma_gronstad/client.cpp
@@ -62,7 +62,7 @@ void client_TCP(){
 
     client_TCP_sock = socket(AF_INET, SOCK_STREAM, 0);
     if(client_TCP_sock < 0){
-        cout<<("Error establishing the server socket"); 
+        cout<<"Error establishing the server socket"; 
         //exit(1);
     }
     
@@ -76,7 +76,7 @@ void client_TCP(){
     
     int bindStatus = bind(client_TCP_sock, (struct sockaddr*) &servAddr, sizeof(servAddr));
     if(bindStatus < 0){
-        cout<<("Error binding socket to local address"); 
+        cout<<"Error binding socket to local address"; 
         //exit(1);
     }
     bzero((char*)&my_addr, sizeof(my_addr));
