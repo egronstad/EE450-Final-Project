65eb251cdd845fe6a3bf97755d2227708104ac57
Emma Gronstad <gronstad@usc.edu>
2022-11-29 04:58:06 +0000

    Add files via upload

--- ee450_gronstad_emma_gronstad/serverEE.cpp
+++ ee450_gronstad_emma_gronstad/serverEE.cpp
@@ -26,7 +26,7 @@ Each backend server should read the corresponding file and store the information
 using namespace std;
 
 //#define DEPTFILE "ee.txt"
-#define EE_PORT    22267
+#define CS_PORT    22267
 #define EE_PORT    23267
 #define M_PORT     24267
 #define BUFSIZE 1024
